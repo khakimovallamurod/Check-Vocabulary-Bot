4a5b7e1d1307086ab5d59dfbb303bfd174b58023
khakimovallamurod <allamurod0206@gmail.com>
2025-02-16 16:48:38 +0200

    correct all

--- handlers.py
+++ handlers.py
@@ -92,10 +92,10 @@ def send_next_image(update: Update, context: CallbackContext):
         random.shuffle(btns)
         reply_markup = InlineKeyboardMarkup(
             [
-                btns[0],
-                btns[1],
-                btns[2],
-                btns[3]
+                [btns[0]],
+                [btns[1]],
+                [btns[2]],
+                [btns[3]]
             ]
         )
 
